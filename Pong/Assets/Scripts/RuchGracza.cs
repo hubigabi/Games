dbc9b4c7e755691bf41b05bf8322884d23fa7978
hubigabi <hubigabi19@gmail.com>
2019-11-24 20:57:34 +0100

    Added quiting game

--- Pong/Assets/Scripts/RuchGracza.cs
+++ Pong/Assets/Scripts/RuchGracza.cs
@@ -20,6 +20,9 @@ public class RuchGracza : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
